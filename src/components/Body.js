0b975e2e9352fd16b4ae5a856681974c79d87c2d
priyank sorathiya <priyanksorathiya07@gmail.com>
2024-05-01 13:24:22 +0530

    add

--- src/components/Body.js
+++ src/components/Body.js
@@ -10,7 +10,7 @@ const Body = () => {
             element: <Login />
         },
         {
-            path: "/header",
+            path: "/header", 
             element: <Header />
         },
     ])
